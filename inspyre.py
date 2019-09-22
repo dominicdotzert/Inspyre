b3038916a0ff3cb3d353f18648aa892cf741da7a
dominicdotzert <dominicdotzert@gmail.com>
2019-09-22 00:08:06 -0400

    Add flat file logging and fix initial delay

--- inspyre.py
+++ inspyre.py
@@ -6,7 +6,6 @@ import time
 import pygame
 
 URL = "https://inspirobot.me/api"
-
 DELAY = 15
 
 pygame.init()
@@ -18,29 +17,33 @@ screen_height = screen.get_height()
 screen_width = screen.get_width()
 img_offset = (screen_width - screen_height) / 2
 
+filename = str(datetime.now()).replace(':', '.') + '.txt'
 t = time.time()
-t = t + DELAY + 1
+t = t - DELAY
 done = False
-while (not done):
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                done = True
-                
-    if (time.time() - t > DELAY):
-        PARAMS = { "generate" : "true"}
-        if (datetime.now().month == 12):
-            PARAMS["season"] = "xmas"
+with open(filename, 'a+') as f:
+	while (not done):
 
-        r = requests.get(url = URL, params = PARAMS)
-        i = requests.get(r.text)
-        img = pygame.image.load(BytesIO(i.content))
-        img = pygame.transform.scale(img, (screen_height, screen_height))
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				done = True
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					done = True
+					
+		if (time.time() - t > DELAY):
+			PARAMS = { "generate" : "true"}
+			if (datetime.now().month == 12):
+				PARAMS["season"] = "xmas"
 
-        screen.blit(img, (img_offset,0))
-        pygame.display.flip()
-        
-        t = time.time()
+			r = requests.get(url = URL, params = PARAMS)
+			f.write(r.text + '\n')
+			i = requests.get(r.text)
+			img = pygame.image.load(BytesIO(i.content))
+			img = pygame.transform.scale(img, (screen_height, screen_height))
+
+			screen.blit(img, (img_offset,0))
+			pygame.display.flip()
+			
+			t = time.time()
